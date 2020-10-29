a710b6c9e54565e8b2f12d5f4c79e9cb0d2f5b85
Alpq <doymazalp@gmail.com>
2020-10-28 22:03:25 -0400

    Added constructor, getBalance(), getAccountID(), setPassword(), deposit(), withdraw() and toString() such that all programs compiled. These functions must all be tested for functionality

---
+++ BankAccount.java
@@ -0,0 +1,47 @@
+public class BankAccount{
+  private double balance = 0;
+  private int accountID;
+  private String password;
+
+  public BankAccount(int ID, String pass)
+  {
+    this.accountID = ID;
+    this.password = pass;
+  }
+  public double getBalance()
+  {
+    return this.balance;
+  }
+  public int getAccountID()
+  {
+    return this.accountID;
+  }
+  public void setPassword(String pass)
+  {
+    this.password = pass;
+  }
+  public boolean deposit(double amount)
+  {
+    boolean truth = amount > 0;
+    if (truth)
+    {
+    this.balance = this.balance + amount;
+      return truth;
+    }
+    return truth;
+  }
+  public boolean withdraw(double amount)
+  {
+    boolean truth = (amount > 0) && (this.balance > amount);
+    if (truth)
+    {
+      this.balance = this.balance - amount;
+      return truth;
+    }
+    return truth;
+  }
+  public String toString()
+  {
+    return Integer.toString(this.accountID) + "\t" + this.balance+"";
+  }
+}
